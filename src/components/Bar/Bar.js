370566ee9998d956a24a8da94dbdb4b2d5ce4644
Sahil Singh <sahils091@gmail.com>
2020-10-14 13:41:38 -0400

    Added on scroll animations for the Bar component

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./bar.scss";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Bar = () => {
   const [spirit, setSpirit] = useState(null);
@@ -14,19 +16,27 @@ const Bar = () => {
         setSpirit(res.data.ingredients[0]);
       });
   };
+  useEffect(() => {
+    Aos.init({
+      duration: 2500,
+    });
+  }, []);
   return (
     <section className="bar">
       <div className="bar__img">
-        <h1 className="bar__title">SCHOOL IS IN SESSION</h1>
+        <h1 className="bar__title" data-aos="fade-up-right">
+          SCHOOL IS IN SESSION
+        </h1>
       </div>
       <div className="bar__form-wrapper">
-        <form className="bar__form" onSubmit={searchByName}>
+        <form className="bar__form" onSubmit={searchByName} data-aos="fade-up">
           <input
             className="bar__field"
             type="text"
             name="search"
             id="search"
             placeholder="Pick Your Poison"
+            data-aos="fade-up-right"
           />
           <button type="submit" className="bar__btn bar__btn--name">
             Search
@@ -35,18 +45,20 @@ const Bar = () => {
 
         {spirit ? (
           <div className="bar__detail-wrapper">
-            <div className="bar__detail">
-              <h2 className="bar__detail-title">
+            <div className="bar__detail" data-aos="fade-up">
+              <h2 className="bar__detail-title" data-aos="fade-up-right">
                 Name: {spirit.strIngredient}
               </h2>
 
-              <h3 className="bar__detail-subtitle">Tags: {spirit.strType}</h3>
+              <h3 className="bar__detail-subtitle" data-aos="fade-up-right">
+                Tags: {spirit.strType}
+              </h3>
 
-              <h3 className="bar__detail-abv">
+              <h3 className="bar__detail-abv" data-aos="fade-up-right">
                 Alcohol Volume (ABV) : {spirit.strABV} %
               </h3>
 
-              <p className="bar__detail-des">
+              <p className="bar__detail-des" data-aos="fade-up-right">
                 {" "}
                 Description : {spirit.strDescription}
               </p>
